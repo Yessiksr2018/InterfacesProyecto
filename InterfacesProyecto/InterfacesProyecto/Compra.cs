c4e92d396aca4ec2f29f490701f3b266df63f823
Yessiksr2018 <38057784+Yessiksr2018@users.noreply.github.com>
2018-05-24 23:05:45 -0500

    Avance
    Avance

---
+++ InterfacesProyecto/InterfacesProyecto/Compra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesProyecto
+{
+    class Compra
+    {
+
+        #region atributos
+        private int compra { get; set; }
+        private int Elemento { get; set; }
+        private int serie { get; set; }
+        private int cantidad { get; set; }
+        private int valor { get; set; }
+
+        #endregion
+    }
+}
---
+++ InterfacesProyecto/InterfacesProyecto/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesProyecto
+{
+    class Persona
+    {
+
+        #region atributos
+        private int usuario { get; set; }
+        private int cedula { get; set; }
+        private String nombre { get; set; }
+        #endregion
+
+    }
+
+   
+   
+   
+}
